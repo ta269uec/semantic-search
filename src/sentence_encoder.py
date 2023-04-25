f50792674da74dd20d67a6246a93a38aa299bb57
tarun joshi <tarun.joshi@gmail.com>
2023-04-25 08:51:03 -0400

    updates to sentence encoder

--- src/sentence_encoder.py
+++ src/sentence_encoder.py
@@ -1,9 +1,9 @@
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, util
      
 class SentenceEncoder():
     def __init__(self, model='sentence-transformers/all-MiniLM-L12-v2', device='cuda'):
         self.model = SentenceTransformer(model, device)
         pass
     
-    def encode(self, sentences):
-        return self.model.encode(sentences)
+    def encode(self, sentences, convert_to_tensor=True):
+        return self.model.encode(sentences, convert_to_tensor=convert_to_tensor)
